5651985d2b6d78b7ae8cbd89d197e3f9db75345e
DUMBRAVA <C.Dumbrava@pentanova.com>
2024-04-01 21:30:16 +0300

    curs7 final

---
+++ src/curs7/ArrayExample.java
@@ -0,0 +1,44 @@
+package curs7;
+
+import java.util.Arrays;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+	String[] textArray = new String[4]; // am creat un array nou cu marimea de 4, trebuie declarat de la inceput marimea in totdeauna nu este redimensionabil
+  //String[]  textArray = {null, null, null, null}
+ //index - mereu de la zero 0     1     2     3
+	System.err.println(textArray[0]);
+	textArray[0] = "Acesta";
+//String[]  textArray = {Acesta, null, null, null}
+//index                     0     1     2     3
+	textArray[1] = " este ";
+	
+	//String[]  textArray = {Acesta, este, null, null}
+	//index                     0     1     2     3
+	textArray[2] = " un ";
+	textArray[3] = " aray ";
+	//String[]  textArray = {Acesta, este, un, array}
+	
+	System.out.println(textArray);
+	
+	System.out.println(Arrays.toString(textArray));
+	                    //din Java
+	
+	System.out.println("-------------------------");
+	
+	for (int i=0; i<textArray.length; i++) {
+		System.out.print(textArray[i]);
+		
+	}
+	
+	System.out.println("\n-------------------------");
+	
+	
+	//for each - pentru ca ia fiecare element in parte
+	for(String element : textArray) {
+		System.out.print(element);
+		
+	}
+}
+}
---
+++ src/curs7/DoWhileExample3.java
@@ -0,0 +1,36 @@
+package curs7;
+/*
+ * validez daca un username nu este nul si nu este mai mic decat 6
+ * daca nu respecta cer din nou
+ * 
+ */
+
+import java.util.Scanner;
+
+public class DoWhileExample3 {
+
+	public static void main(String[] args) {
+		
+		//Scanner scan = new Scanner(System.in);
+		
+		//System.out.println("Introdu un username: ");
+		
+		//String username = scan.next();
+		
+		//while (username.equals(null) || username.length() <6) {
+		//	System.out.println("Introdu un username: ");
+		//	String username = scan.next();
+			
+		//}
+		
+		//System.out.println("---------- ");
+
+
+	//String username2;
+	//do {
+	//	System.out.println("Introdu un username: ");
+	//	String username2 = scan.next();
+		
+	//}while (username2.equals(null) || username2.length() <6);
+}
+}
